20990b0e9b77165e21d1127bf822acaa24a86726
Pedro G <pgas2003@gmail.com>
2025-02-09 20:54:36 -0300

    Bug de commit corrigido

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,6 +11,7 @@ const buscasRouter= require('./routes/buscas.js');
 const mostViewedRouter= require('./routes/mostViewed.js');
 //const usersRouter = require('./src/routes/gerenciaUsuarios.js');
 //const reviewsRouter = require('./src/routes/reviews');
+const relatoriosRoutes = require("./routes/relatorios"); // Importando a rota de relatórios
 
 // Montagem das rotas
 app.use('/dishes', pratosRouter);
@@ -19,6 +20,7 @@ app.use('/search', buscasRouter);
 app.use('/most-viewed', mostViewedRouter);
 //app.use('/users', usersRouter);
 //app.use('/reviews', reviewsRouter);
+app.use("/relatorios", relatoriosRoutes); // Adicionando a rota ao servidor
 
 // Inicialização do servidor
 app.listen(PORT, () => {
