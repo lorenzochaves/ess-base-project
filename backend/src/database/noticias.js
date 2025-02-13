bc62af6ecc7ce8e2d68f69a0bae00ae4444b0772
RafaRamosBC <rafaelramoscorcino@hotmail.com>
2025-02-12 21:36:56 -0300

    Create noticias.js
    database de noticias

---
+++ backend/src/database/noticias.js
@@ -0,0 +1,28 @@
+const news = [
+    {
+      title: "Novo cardápio no RU UFPE",
+      subtitle: "Confira as novidades desta semana no restaurante universitário.",
+      body: "O restaurante universitário da UFPE está com um novo cardápio esta semana, incluindo pratos especiais como frango grelhado com legumes e lasanha de berinjela. Venha experimentar!",
+      publicationDate: "01/10/2023"
+    },
+    {
+      title: "Promoção de fim de mês no RU",
+      subtitle: "Aproveite descontos especiais nos pratos selecionados.",
+      body: "Para celebrar o fim do mês, o RU UFPE está oferecendo descontos de até 20% em pratos selecionados, como strogonoff de carne e risoto de cogumelos. A promoção é válida até o dia 31/10.",
+      publicationDate: "25/10/2023"
+    },
+    {
+      title: "RU UFPE adota medidas sustentáveis",
+      subtitle: "Restaurante universitário implementa práticas ecológicas.",
+      body: "O RU UFPE está adotando medidas sustentáveis, como a redução do uso de plásticos e a implementação de compostagem para os resíduos orgânicos. A iniciativa visa reduzir o impacto ambiental e promover a conscientização.",
+      publicationDate: "15/10/2023"
+    },
+    {
+      title: "Avaliação do RU UFPE",
+      subtitle: "Participe da pesquisa de satisfação do restaurante universitário.",
+      body: "O RU UFPE está realizando uma pesquisa de satisfação para melhorar a qualidade dos serviços e pratos oferecidos. Participe e dê sua opinião! A pesquisa está disponível no site do restaurante até o dia 30/10.",
+      publicationDate: "20/10/2023"
+    }
+  ];
+
+module.exports = { news };
